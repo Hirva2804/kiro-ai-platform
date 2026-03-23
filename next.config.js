277ca4b397da6f5c604b85863b5770a8745e1ced
Your Name <your@email.com>
2026-03-23 23:43:23 +0530

    fix: resolved build errors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // required for react-beautiful-dnd
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
 }
 
 module.exports = nextConfig
